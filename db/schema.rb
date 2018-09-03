d608cc25ab062dcd3eabd8502ddd12114b8cffc3
吉本光輝 <>
2018-09-03 19:40:05 +0900

    :art: style header

--- db/schema.rb
+++ db/schema.rb
@@ -7,7 +7,6 @@
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
-#
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_09_03_073354) do
